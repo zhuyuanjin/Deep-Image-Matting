0781f7d497a772526efde23519b8e304397ba119
syxz1995 <syxz1995@gmail.com>
2020-01-26 10:57:03 +0800

    DataSet for Human

--- DataSet.py
+++ DataSet.py
@@ -10,74 +10,31 @@ from scipy import misc
 from PIL import Image
 
 class MattingDataSet(Dataset):
-    def __init__(self, fg_path, a_path, bg_path, merged_path):
-        self.fg_path = fg_path
+    def __init__(self, img_path, a_path):
+        self.img_path = img_path
         self.a_path = a_path
-        self.bg_path = bg_path
-        self.merged_path = merged_path
-        self.merged_lst = os.listdir(self.merged_path)
+        self.img_lst = os.listdir(self.img_path)	
 
     def __len__(self):
 
-        return len(self.merged_lst)
+        return len(self.img_lst)
 
     def __getitem__(self, item):
-        #Load Data from Disk
-        img_name, _ = self.merged_lst[item].split('.')
-        fg_name, bg_name = img_name.split('+')
-        img = Image.open(self.merged_path + img_name + '.png')
-        fg = Image.open(self.fg_path + fg_name + '.jpg')
-        bg = Image.open(self.bg_path + bg_name + '.jpg')
-        alpha = Image.open(self.a_path + fg_name + '.png')
-
-        #Convert to RGB if not
-        if bg.mode != 'RGB' and bg.mode != 'RGBA':
-            bg = bg.convert('RGB')
-        if img.mode != 'RGB' and img.mode != 'RGBA':
-            img = img.convert('RGB')
-
-        #if bg is smaller than fg, resize bg to assert bg.size >= fg.size
-        bg_bbox = bg.size
-        bw = bg_bbox[0]
-        bh = bg_bbox[1]
-        w, h = img.size
-        wratio = w / bw
-        hratio = h / bh
-        ratio = wratio if wratio > hratio else hratio
-        if ratio > 1:
-            bg = bg.resize((math.ceil(bw*ratio),math.ceil(bh*ratio)), Image.BICUBIC)
-
-
-        #Convert the PIL.Image format to numpy.array
-        fg = np.array(fg)
-        bg = np.array(bg)
-        alpha = np.array(alpha)
+        img_name, _ = self.img_lst[item].split(".")
+        alpha = np.array(Image.open(os.path.join(self.a_path, img_name+'.png')))
+        img = np.array(Image.open(os.path.join(self.img_path, img_name+'.jpg')))
         _kernel_size = np.random.randint(3,8,1).item()
         trimap = generate_trimap(alpha, (_kernel_size, _kernel_size))
-        img = np.array(img)
-        #crop the images
-        ##get the location of the bounding box
         loc = random_sample(img, trimap)
-
         img_patch = data_crop(img, loc)
-        fg_patch = data_crop(fg, loc)
         trimap_patch = data_crop(trimap, loc)
         alpha_patch = data_crop(alpha, loc)
-        bg_patch = data_crop(bg, loc)
-
-        #generate the bool matrix to locate the unknown region note that trimap has not / 255.0
         unknown = binary_unknown(trimap_patch)[np.newaxis, :, :]
-
-        ## covert the dim to the right order and right type
-
         img_patch = img_patch.transpose(2, 0, 1) / 255.0
         alpha_patch = alpha_patch[np.newaxis, :, :] / 255.0
         trimap_patch = trimap_patch[np.newaxis, :, :] / 255.0
-        fg_patch = fg_patch.transpose(2, 0, 1) / 255.0
-        bg_patch = bg_patch.transpose(2, 0, 1) / 255.0
+        return {'img': img_patch,   'trimap': trimap_patch, 'alpha': alpha_patch, 'unknown':unknown}
 
-
-        return {'img': img_patch, 'fg': fg_patch, 'bg': bg_patch, 'trimap': trimap_patch, 'alpha': alpha_patch, 'unknown':unknown}
 
 
 
